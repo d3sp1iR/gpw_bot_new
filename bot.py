65232e804c826065ad850d9d2726d328f346ee36
d3sp1iR <relersad@gmail.com>
2026-05-05 00:09:30 +0300

    docker fixed again

--- bot.py
+++ bot.py
@@ -1,10 +1,11 @@
 import telebot
 from config import BOT_TOKEN
+print("STEP 1")
 import handlers
 
 
 bot = telebot.TeleBot(BOT_TOKEN)
-
+print("STEP 2")
 handlers.register(bot)
 
 print("Bot started...")
